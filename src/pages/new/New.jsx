42a3ea4537f87a47d5c3cc87b34fc37a7ec15997
Papun Mohanty <papun@BOOK-Q002T004VI.localdomain>
2022-10-25 14:59:55 +0530

    Add: User single page

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,9 +1,17 @@
-import "./new.scss"
+import "./new.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
 
 const New = () => {
-    return (
-        <div>New</div>
-    )
-}
+  return (
+    <div className="new">
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        test
+      </div>
+    </div>
+  );
+};
 
-export default New
+export default New;
