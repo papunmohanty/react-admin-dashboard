dee5ebf9ea1378d7a05fa588520ddbb8178b6ae5
Papun Mohanty <papun@BOOK-Q002T004VI.localdomain>
2022-11-21 01:33:11 +0530

    Mofify: console log

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
         navigate("/");
       })
       .catch((error) => {
-        console.log(error.response.data);
+        console.log(error);
         setError(true);
       });
   };
