47c6c11262cf94602303ec674cab581382b10501
Papun Mohanty <papun@BOOK-Q002T004VI.localdomain>
2022-11-21 00:56:47 +0530

    Add Login Page and Axios Login Post Request to Backend

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,52 @@
-import "./login.scss"
+import { useContext, useState } from "react";
+import "./login.scss";
+
+import { Navigate, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
-}
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://127.0.0.1:8000/login", {
+        username: email,
+        password: password,
+      })
+      .then((result) => {
+        console.log(`Access token ${result.data.access_token}`);
+        localStorage.setItem("token", result.data.access_token);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        setError(true);
+      });
+  };
+
+  return (
+    <div className="login">
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+        {error && <span>Wrong email or password!</span>}
+      </form>
+    </div>
+  );
+};
+
+export default Login;
